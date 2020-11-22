d36a7cfd2e73f3e63064405fdddea3d47a24ae3d
Steven crangle <electricnoodle7@googlemail.com>
2020-11-22 17:57:01 +0000

    Added host specific filter to traffic trace, also added struct to store basic flow info, and map to store unique flows over time.

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net"
 	"os"
 	"time"
 
@@ -22,22 +23,44 @@ import (
 	"github.com/google/gopacket/pcap"
 )
 
-var iface = flag.String("i", "eth0", "Interface to read packets from")
+var iface = flag.String("i", "wlp2s0", "Interface to read packets from")
 
 var handle *pcap.Handle
-var promisc = true
+var promisc = false
+var statsTime int
+
+/*PacketFlow used to store information on each distinct flow of packets */
+type PacketFlow struct {
+	packets    []*layers.IPv4
+	srcIP      net.IP
+	dstIP      net.IP
+	srcPort    uint16
+	dstPort    uint16
+	protocol   string
+	totalBytes int64
+}
+
+var hostIP net.IP
+var packetFlows map[string]*PacketFlow
 
 func main() {
 	defer util.Run()()
 
-	handle = initPCAPHandle()
-	getPackets(handle)
+	statsTime = 20
+	hostIP = getAddressInfo(*iface)
+
+	handle = initPCAPHandle(hostIP)
+	packetFlows = make(map[string]*PacketFlow)
+
+	fmt.Printf("IP: %s\n", hostIP.String())
+
+	getPackets(handle, statsTime)
 
 	defer handle.Close()
 
 }
 
-func initPCAPHandle() *pcap.Handle {
+func initPCAPHandle(ip net.IP) *pcap.Handle {
 
 	var err error
 
@@ -62,10 +85,46 @@ func initPCAPHandle() *pcap.Handle {
 
 	}
 
+	var filter string = "host " + ip.String()
+	err = handle.SetBPFFilter(filter)
+
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
 	return handle
 }
 
-func getPackets(src gopacket.PacketDataSource) {
+func getAddressInfo(interfaceName string) net.IP {
+
+	i, err := net.InterfaceByName(interfaceName)
+
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	addresses, err := i.Addrs()
+
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	var ip net.IP
+
+	switch v := addresses[0].(type) {
+	case *net.IPNet:
+		ip = v.IP
+	case *net.IPAddr:
+		ip = v.IP
+	}
+	return ip
+
+}
+
+func getPackets(src gopacket.PacketDataSource, stats int) {
 
 	var dec gopacket.Decoder
 	var ok bool
@@ -85,10 +144,7 @@ func getPackets(src gopacket.PacketDataSource) {
 
 	count := 0
 	bytes := int64(0)
-	//start := time.Now()
-	//errors := 0
-	//truncated := 0
-	//layertypes := map[gopacket.LayerType]int{}
+
 	defragger := ip4defrag.NewIPv4Defragmenter()
 
 	for packet := range source.Packets() {
@@ -96,37 +152,100 @@ func getPackets(src gopacket.PacketDataSource) {
 		count++
 		bytes += int64(len(packet.Data()))
 
-		/* Handles defragmentation */
-		ip4Layer := packet.Layer(layers.LayerTypeIPv4)
+		processPacket(packet, defragger)
 
-		if ip4Layer == nil {
-			continue
-		}
+		if count%stats == 0 {
 
-		ip4 := ip4Layer.(*layers.IPv4)
-		l := ip4.Length
+			fmt.Printf("Processed %v packets (%v bytes).\n", count, bytes)
+			fmt.Printf("Packet Flows: \n")
 
-		newip4, err := defragger.DefragIPv4(ip4)
-
-		if err != nil {
-			log.Fatalln("Error while de-fragmenting", err)
-		} else if newip4 == nil {
-			continue // packet fragment, we don't have whole packet yet.
-		}
-		if newip4.Length != l {
-
-			fmt.Printf("Decoding re-assembled packet: %s\n", newip4.NextLayerType())
-			pb, ok := packet.(gopacket.PacketBuilder)
-			if !ok {
-				panic("Not a PacketBuilder")
+			for _, flow := range packetFlows {
+				fmt.Printf("Source: %s\t%d\tDest: \t%s\t%d\tProtocol: %s\tTotalBytes: %d\n",
+					flow.srcIP.String(),
+					flow.srcPort,
+					flow.dstIP.String(),
+					flow.dstPort,
+					flow.protocol,
+					flow.totalBytes)
 			}
-			nextDecoder := newip4.NextLayerType()
-			nextDecoder.Decode(newip4.Payload, pb)
-
+			fmt.Printf("%d Unique flows.\n", len(packetFlows))
 		}
-
-		fmt.Println(packet.Dump())
-		fmt.Println(packet)
-
 	}
+}
+
+func processPacket(packet gopacket.Packet, defragger *ip4defrag.IPv4Defragmenter) {
+
+	/* Handles defragmentation */
+	ip4Layer := packet.Layer(layers.LayerTypeIPv4)
+
+	if ip4Layer == nil {
+		return
+	}
+
+	ip4 := ip4Layer.(*layers.IPv4)
+	newip4, err := defragger.DefragIPv4(ip4)
+
+	if err != nil {
+		log.Fatalln("Error while de-fragmenting", err)
+	} else if newip4 == nil {
+		return
+	}
+
+	if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
+
+		processFlow(packet, newip4, tcpLayer, "TCP")
+
+	} else if udpLayer := packet.Layer(layers.LayerTypeUDP); udpLayer != nil {
+
+		processFlow(packet, newip4, udpLayer, "UDP")
+	}
+
+}
+
+func processFlow(packet gopacket.Packet, ipv4 *layers.IPv4, layer gopacket.Layer, protocol string) {
+
+	var key1 string
+	var key2 string
+
+	var flow *PacketFlow
+
+	if protocol == "TCP" {
+
+		tcp, _ := layer.(*layers.TCP)
+
+		key1 = protocol + ":" + ipv4.SrcIP.String() + ":" + tcp.SrcPort.String() + ":" + ipv4.DstIP.String() + ":" + tcp.DstPort.String()
+		key2 = protocol + ":" + ipv4.DstIP.String() + ":" + tcp.DstPort.String() + ":" + ipv4.SrcIP.String() + ":" + tcp.SrcPort.String()
+
+		flow = &PacketFlow{nil, ipv4.SrcIP, ipv4.DstIP, uint16(tcp.SrcPort), uint16(tcp.DstPort), protocol, int64(len(packet.Data()))}
+
+	} else if protocol == "UDP" {
+
+		udp, _ := layer.(*layers.UDP)
+
+		key1 = protocol + ":" + ipv4.SrcIP.String() + ":" + udp.SrcPort.String() + ":" + ipv4.DstIP.String() + ":" + udp.DstPort.String()
+		key2 = protocol + ":" + ipv4.DstIP.String() + ":" + udp.DstPort.String() + ":" + ipv4.SrcIP.String() + ":" + udp.SrcPort.String()
+
+		flow = &PacketFlow{nil, ipv4.SrcIP, ipv4.DstIP, uint16(udp.SrcPort), uint16(udp.DstPort), protocol, int64(len(packet.Data()))}
+	}
+
+	if element, found := packetFlows[key1]; found {
+
+		element.totalBytes += int64(len(packet.Data()))
+		packetFlows[key1] = element
+
+	} else if element, found := packetFlows[key2]; found {
+
+		element.totalBytes += int64(len(packet.Data()))
+		packetFlows[key2] = element
+
+	} else {
+
+		if hostIP.Equal(ipv4.SrcIP) {
+			packetFlows[key1] = flow
+		}
+		if hostIP.Equal(ipv4.DstIP) {
+			packetFlows[key2] = flow
+		}
+	}
+
 }
